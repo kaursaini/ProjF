a612fc25a4dfed9b850a1e79b260047ef21a4e83
Harpreet Saini <23harpreet08kaursaini@gmail.com>
2018-12-15 01:34:24 -0500

    Project F Angular Client App

---
+++ app/global.ts
@@ -0,0 +1,6 @@
+export class Global
+{
+  public static readonly PRIME_URL = "http://localhost:4413/ProjF/Prime.do";
+  public static readonly SIS_URL = "http://localhost:4413/ProjF/Sis.do";
+  public static readonly WORLD_URL = "https://www.eecs.yorku.ca/~roumani/server$
+}
